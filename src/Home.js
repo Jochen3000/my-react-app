ff0ad8ddb256fa830f7052dab771f72cb9617722
jochen ade <jochen.ade@springer.com>
2022-08-02 14:40:41 +0200

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-             <h2>Homepage</h2>
-        </div>
+     const [blogs, setBlogs] = useState([
+          { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+          { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+          { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+     ]);
+     return (
+          <div className="home">
+               {blogs.map((blog) => (
+                    <h1>{blog.title}</h1>
+               ))}
+          </div>
      );
 }
- 
+
 export default Home;
