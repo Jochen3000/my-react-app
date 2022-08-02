639966fdf9a7bfc1779223223ca156251fc2a4d8
jochen ade <jochen.ade@springer.com>
2022-08-02 15:29:28 +0200

    minor

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,9 @@
-import { useState } from "react";
+import Bloglist from "./Bloglist";
 
 const Home = () => {
-     const [blogs, setBlogs] = useState([
-          { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-          { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-          { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-     ]);
      return (
           <div className="home">
-               {blogs.map((blog) => (
-                    <h1>{blog.title}</h1>
-               ))}
+               <Bloglist />
           </div>
      );
 }
